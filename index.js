e4fbca050e691706e34e805570db7c79c15fec0b
drakenevadie19 <drakennievadje19@gmail.com>
2024-07-01 00:13:45 -0500

    New Change: Done with all functions

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const { redirect } = require('express/lib/response');
 const app = express();
+const bodyParser = require('body-parser');
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // In-memory database simulation (hashmap)
 const urlDb = new Map();
@@ -26,34 +31,34 @@ app.get('/api/hello', function(req, res) {
 // Function to generate URL number param
 function shortenURL(longUrl) {
   let key = Math.floor(Math.random()*3000);
-  while (!urlDb.has(key)) { // Ensure the key is unique
+  while (urlDb.has(key)) { // Ensure the key is unique
     key = Math.floor(Math.random()*3000);
   }
+  urlDb.set(key, longUrl);
   return key;
 }
 
 // Map URL to a short URL
 app.get('/api/shorturl/:number', (req, res) => {
-  if (urlDb.has(req.params.number)) {
-    redirect(urlDb.get(req.params.number));
+  const key = parseInt(req.params.number, 10);
+  if (urlDb.has(key)) {
+    redirect(urlDb.get(key));
   } else {
     res.status(400).send('This shorten URL was not mapped to any URL');
   }
 })
 
 // api for URL shortener
-app.post("/api/shorturl", (req, res) => {
-  console.log(req.body);
-  const longUrl = "";
+app.post('/api/shorturl', (req, res) => {
+  const longUrl = req.body.url;
+  // const longUrl = ""; 
   // const longUrl = req.body.url_input;
-  // if (!longUrl) {
-  //   return res.send('URL is required');
-  // }
+  if (!longUrl) {
+    return res.status(400).send('URL is required');
+  }
 
   const numberMatch = shortenURL(longUrl);
   console.log(numberMatch);
-  urlDb.set(numberMatch, longUrl);
-
   res.send({ "original_url":longUrl, "short_url":numberMatch })
 });
 
